5574c67bad0104a1ed009cf68ec34b98d232bc5b
David Lafferty <dcl257@cornell.edu>
2025-03-05 15:13:01 -0500

    area overlap for load modeling

--- src/climate_utils.py
+++ src/climate_utils.py
@@ -1,6 +1,7 @@
 import salem
 import geopandas as gpd
 import pandas as pd
+import dask
 
 from python_utils import project_path
 
@@ -11,48 +12,48 @@ def tgw_to_zones(
     nyiso_zone_shp_path: str = f"{project_path}/data/nyiso/shapefiles/NYISO_Load_Zone_Dissolved.shp",
 ) -> pd.DataFrame:
     """
-    Converts TGW output variables to NYISO load zone (weighted) averages.
+    Converts TGW output variables to NYISO load zone (area weighted) averages.
 
     Code based on: https://github.com/IMMM-SFA/im3components/blob/main/im3components/wrf_to_tell/wrf_tell_counties.py
     """
 
     # Read TGW
-    tgw = salem.open_wrf_dataset(tgw_file_path)
+    tgw = salem.open_wrf_dataset(tgw_file_path)[tgw_vars]
+    tgw = tgw.where((tgw.lat > 40) & (tgw.lon > -80), drop=True) # subset NYS
+    
     tgw_crs = tgw.pyproj_srs
+    geometry = tgw.salem.grid.to_geometry().geometry
 
     # Read NYISO zones
     gdf = gpd.read_file(nyiso_zone_shp_path).to_crs(tgw_crs)
 
-    # Loop through times
-    out = []
-    for itime in range(len(tgw["time"])):
-        # Convert TGW to dataframe
-        tgw_df = tgw[tgw_vars].isel(time=itime).to_dataframe().reset_index(drop=True)
-        tgw_df = gpd.GeoDataFrame(
-            tgw_df,
-            geometry=tgw.salem.grid.to_geometry().geometry,
-        ).set_crs(tgw_crs)
-        tgw_df["cell_index"] = tgw_df.index.values
+    # Get the intersection df
+    tgw_df_single = tgw.isel(time=0).to_dataframe().reset_index(drop=True)
+    tgw_df_single = gpd.GeoDataFrame(tgw_df_single, geometry=geometry).set_crs(tgw_crs)
+    intersection_single = gpd.overlay(gdf, tgw_df_single, how="intersection")
 
-        # Get intersection
-        intersection = gpd.overlay(gdf, tgw_df, how="intersection")
+    # Convert TGW to dataframe
+    tgw_df = tgw.to_dataframe().reset_index()
 
-        # Area weighting
-        intersection["area"] = intersection.area
-        intersection["weight"] = intersection["area"] / intersection[["ZONE", "area"]].groupby("ZONE").area.transform(
-            "sum"
-        )
+    # Merge
+    intersection = pd.merge(tgw_df[['time', 'lat', 'lon'] + tgw_vars],
+                            intersection_single[['ZONE', 'lat', 'lon', 'geometry']], 
+                            how = 'inner', on=['lat', 'lon'])
+    intersection = gpd.GeoDataFrame(intersection, geometry=intersection['geometry'])
 
-        # Compute average
-        out.append(
-            intersection[tgw_vars]
-            .multiply(
-                intersection["weight"],
-                axis="index",
-            )
-            .join(intersection[["ZONE", "time"]])
-            .groupby(["ZONE", "time"])
-            .sum()
-        )
+    # Area weighting
+    intersection["area"] = intersection.area
+    intersection["weight"] = (intersection["area"] / 
+                    intersection[["ZONE", 'time', "area"]]
+                    .groupby(["ZONE", 'time']).area.transform("sum"))
 
-    return pd.concat(out)
+    # Compute area-weighted average
+    out = (
+        intersection[tgw_vars]
+        .multiply(intersection["weight"], axis="index")
+        .join(intersection[["ZONE", "time"]])
+        .groupby(["ZONE", "time"])
+        .sum()
+    )
+
+    return out
